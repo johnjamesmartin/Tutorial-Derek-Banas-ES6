a36a9cd54154195db4bce86888d24a15aae29c5b
“John <johnjamesmartin@live.com>
2017-01-07 23:13:41 +0000

    16. Symbols

--- js/ecmatut.js
+++ js/ecmatut.js
@@ -321,3 +321,26 @@ class Koala extends getClass(2) {
 
 let carl = new Koala('Carl');
 document.write(`${carl.getInfo()}<br/>`);
+
+
+
+/* 17. Symbols
+ ***************************************************************************************/
+
+let capital = Symbol('State capital');
+
+let pennsylvania = {};
+pennsylvania[capital] = 'Harrisburg';
+document.write(`Capital of PA: ${pennsylvania[capital]}<br/>`);
+document.write(`Symbol capital: ${capital.toString()}<br/>`);
+
+let employNum = Symbol.for('Employee Number');
+
+let bobSmith = {};
+bobSmith[employNum] = 10;
+
+let sallyMarks = {};
+sallyMarks[employNum] = 11;
+
+document.write(`Bob: ${bobSmith[employNum]}<br/>`);
+document.write(`Sally: ${sallyMarks[employNum]}<br/>`);
