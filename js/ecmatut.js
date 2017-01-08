94ccc96f5f925bc52b6e9d579c1050e3c71a8768
“John <johnjamesmartin@live.com>
2017-01-08 22:38:56 +0000

    17. Arrays

--- js/ecmatut.js
+++ js/ecmatut.js
@@ -344,3 +344,19 @@ sallyMarks[employNum] = 11;
 
 document.write(`Bob: ${bobSmith[employNum]}<br/>`);
 document.write(`Sally: ${sallyMarks[employNum]}<br/>`);
+
+
+
+/* 18. Arrays
+ ***************************************************************************************/
+
+let array1 = Array.of(1, 2, 3);
+
+// array from string:
+let array2 = Array.from('word');
+
+let array3 = Array.from(array1, (value) => value * 2);
+
+for (let val of array1) document.write(`Array value: ${val}<br/>`);
+for (let val of array2) document.write(`Array value: ${val}<br/>`);
+for (let val of array3) document.write(`Array value: ${val}<br/>`);
